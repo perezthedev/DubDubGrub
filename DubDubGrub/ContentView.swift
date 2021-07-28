3c37af7694b44fa2fd8b3209c643e5f9a334942b
Ryan Perez <ryancs115@gmail.com>
2021-07-27 21:23:45 -0700

    Initial Commit

---
+++ DubDubGrub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DubDubGrub
+//
+//  Created by thepercussivedev on 7/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by thepercussivedev on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
