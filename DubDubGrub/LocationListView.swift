6d32adf1288a4f114a345d42a1764dd601b3cbc4
Ryan Perez <ryancs115@gmail.com>
2021-08-02 15:57:24 -0700

    LocationCell in its own file

--- DubDubGrub/LocationListView.swift
+++ DubDubGrub/LocationListView.swift
@@ -41,33 +41,4 @@ struct AvatarView: View {
     }
 }
 
-struct LocationCell: View {
-    var body: some View {
-        HStack {
-            Image("default-square-asset")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 80, height: 80)
-                .clipShape(Circle())
-                .padding(.vertical, 8)
-            
-            VStack (alignment: .leading) {
-                Text("Test Location name")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .lineLimit(1)
-                    .minimumScaleFactor(0.75)
-                
-                HStack {
-                    AvatarView(size: 35)
-                    AvatarView(size: 35)
-                    AvatarView(size: 35)
-                    AvatarView(size: 35)
-                    AvatarView(size: 35)
-                    
-                }
-            }
-            .padding(.leading)
-        }
-    }
-}
+
