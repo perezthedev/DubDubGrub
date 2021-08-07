57e3d080a352d9d219f9c1116b0dba6d37ad97c3
Ryan Perez <ryancs115@gmail.com>
2021-08-06 20:47:38 -0700

    DDGProfile swift file

---
+++ DubDubGrub/Model/DDGProfile.swift
@@ -0,0 +1,37 @@
+//
+//  DDGProfile.swift
+//  DubDubGrub
+//
+//  Created by thepercussivedev on 8/6/21.
+//
+
+import CloudKit
+
+struct DDGProfile {
+    
+    // names here, need to match exactly what was typed in CloudKit dashboard
+    static let kFirstName = "firstName"
+    static let kLastName = "lastName"
+    static let kAvatar = "avatar"
+    static let kCompanyName = "companyName"
+    static let kBio = "bio"
+    static let kIsCheckedIn = "isCheckedIn"
+    
+    let ckRecordID: CKRecord.ID
+    let firstName: String
+    let lastName: String
+    let avatar: CKAsset!
+    let companyName: String
+    let bio: String
+    let isCheckedIn: CKRecord.Reference? = nil
+    
+    init(record: CKRecord) {
+        ckRecordID = record.recordID
+        firstName = record[DDGProfile.kFirstName] as? String ?? "N/A"
+        lastName = record[DDGProfile.kLastName] as? String ?? "N/A"
+        avatar = record[DDGProfile.kAvatar] as? CKAsset
+        companyName = record[DDGProfile.kCompanyName] as? String ?? "N/A"
+        bio = record[DDGProfile.kBio] as? String ?? "N/A"
+    }
+    
+}
