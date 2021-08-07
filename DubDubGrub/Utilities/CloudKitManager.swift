288819ef3acda766b48c41ca2c495d57f15629a1
Ryan Perez <ryancs115@gmail.com>
2021-08-07 17:12:49 -0400

    added Constants file

--- DubDubGrub/Utilities/CloudKitManager.swift
+++ DubDubGrub/Utilities/CloudKitManager.swift
@@ -9,4 +9,9 @@ import CloudKit
 
 struct CloudKitManager {
     
+    //standard network call
+    static func getLocations(completed: @escaping (Result<[DDGLocation], Error>) -> Void) {
+        let query = CKQuery(recordType: "DDGLocation", predicate: NSPredicate(value: true))
+    }
+    
 }
---
+++ DubDubGrub/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  DubDubGrub
+//
+//  Created by thepercussivedev on 8/7/21.
+//
+
+import Foundation
+
+enum RecordType {
+    static let location = "DDGLocation"
+    static let profile = "DDGProfile"
+}
