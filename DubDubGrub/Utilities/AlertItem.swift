cb7a8c98cd20b1ec5ce464ac4b3a08a54f900640
Ryan Perez <ryancs115@gmail.com>
2021-08-09 01:09:27 -0400

    updated LocationMapView and tested alertItem location

--- DubDubGrub/Utilities/AlertItem.swift
+++ DubDubGrub/Utilities/AlertItem.swift
@@ -9,4 +9,15 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+    
+}
+
+struct AlertContext {
+    //Mark  -MapView Errors
+    static let unableToGetLocations = AlertItem(title: Text("Locations Error"),
+                                                message: Text("Unable to retrieve locations at this time. \nPlease try again."),
+                                                dismissButton: .default(Text("Okay")))
 }
--- DubDubGrub/Views/Screens/LocationMapView.swift
+++ DubDubGrub/Views/Screens/LocationMapView.swift
@@ -11,6 +11,9 @@ import MapKit
 struct LocationMapView: View {
     
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)) // span is how zoomed in/out you are on the map
+    
+    @State private var alertItem: AlertItem?
+    
     var body: some View {
         ZStack {
             Map(coordinateRegion: $region).ignoresSafeArea()
@@ -20,13 +23,16 @@ struct LocationMapView: View {
                 Spacer()
             }
         }
+        .alert(item: $alertItem, content: { alertItem in
+            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+        })
         .onAppear {
             CloudKitManager.getLocations { result in
                 switch result {
                 case .success(let locations):
                     print(locations)
-                case .failure(let error):
-                    print(error.localizedDescription)
+                case .failure(_):
+                    alertItem = AlertContext.unableToGetLocations
                 }
             }
         }
